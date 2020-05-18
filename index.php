5f905317347eac4f73f03feea59e9d6a10f324d9
Derek Teague <derek@randompherret.com>
2020-05-18 16:47:50 -0500

    Enable answering intial challenge

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
+require_once 'logic/config.php';
+require_once 'logic/functions.php';
+require_once 'logic/slack.php';
+$request = file_get_contents("php://input");
+$event = json_decode($request, TRUE);
 
+if (isset($event['challenge'])){
+    echo $event['challenge'];
+}
 ?>
