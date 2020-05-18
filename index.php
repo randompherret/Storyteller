fb166c83dd97e18b30e5e17e23506de7cee1ceb6
Derek Teague <derek@randompherret.com>
2020-05-18 16:51:16 -0500

    Get request headers, confirm request came from slack, confirm message is recent, confim request came from our app

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 require_once 'logic/config.php';
 require_once 'logic/functions.php';
 require_once 'logic/slack.php';
+$headers = getallheaders();
 $request = file_get_contents("php://input");
 $event = json_decode($request, TRUE);
 
 if (isset($event['challenge'])){
     echo $event['challenge'];
+if (isset($headers['X-Slack-Request-Timestamp'], $headers['X-Slack-Signature'])){
+    $requestSignature = $headers['X-Slack-Signature'];
+    $eventVersion = explode("=", $requestSignature)[0];
+    $timestamp = $headers['X-Slack-Request-Timestamp'];
+    $token = $event['token'];
+    $timediff = time() - $timestamp;
+    if (abs($timediff) >= 60){
+        die("Message time incorrect)");
+    }
+    $eventHash = hash_hmac('sha256', "$eventVersion:$timestamp:$request", $config->slack_secret);
+    if(!hash_equals($requestSignature, "$eventVersion=$eventHash")){
+        die("No Spoofing");
+    }
+} else {
+    die("Not slack");
 }
+
 ?>
