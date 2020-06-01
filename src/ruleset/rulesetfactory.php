72e31134194458ba8e5dcebd7f123aca2b5d562f
Derek Teague <derek@randompherret.com>
2020-05-31 20:41:25 -0500

    Start command stucture

--- src/ruleset/rulesetfactory.php
+++ src/ruleset/rulesetfactory.php
@@ -4,4 +4,8 @@ namespace src\Ruleset;
 use \src\Dice\Roller;
 
 abstract class rulesetFactory {
+    private $commands;
+    public function __construct() {
+        $this->commands = array();
+    }    
 }
