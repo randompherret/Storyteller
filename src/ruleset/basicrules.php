d46136787b65b5b85b09fd365a129a73fd3a2bbb
Derek Teague <derek@randompherret.com>
2020-05-31 20:39:07 -0500

    Start rulesets

---
+++ src/ruleset/basicrules.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace src\RuleSet;
+use src\Ruleset\RuleSetFactory;
+
+class basicRules extends RuleSetFactory {
+    
+}
---
+++ src/ruleset/rulesetfactory.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace src\Ruleset;
+use \src\Dice\Roller;
+
+abstract class rulesetFactory {
+}
