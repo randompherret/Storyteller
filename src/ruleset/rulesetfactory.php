02b7a5996b98b85be19f939e86f41b64612498af
Derek Teague <derek@randompherret.com>
2020-05-31 20:42:49 -0500

    create roller command

--- src/ruleset/rulesetfactory.php
+++ src/ruleset/rulesetfactory.php
@@ -5,13 +5,22 @@ use \src\Dice\Roller;
 
 abstract class rulesetFactory {
     private $commands;
+    
     public function __construct() {
         $this->commands = array();
+        $this->roller = new roller();
+        $this->commands["roll"] = array(
+            "command" => "rollDice",
+            "hint" => "use format 1d6+2 and get random numbers!",
+        );
         $this->commands["help"] = array(
             "command" => "getHelp",
             "hint" => "Show this help.",
         );
     }    
+    public function rollDice(string $dice): string {
+        return "rolled {$this->roller->roll($dice)}";
+    }
     public function getHelp(): string {
         $message = "Here are the commands you can run:\n";
         ksort($this->commands);
