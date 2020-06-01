81f2db9e8701cacf20f821b95dcd5a16f0a47e5e
Derek Teague <derek@randompherret.com>
2020-05-31 20:42:14 -0500

    create help command

--- src/ruleset/rulesetfactory.php
+++ src/ruleset/rulesetfactory.php
@@ -7,5 +7,18 @@ abstract class rulesetFactory {
     private $commands;
     public function __construct() {
         $this->commands = array();
+        $this->commands["help"] = array(
+            "command" => "getHelp",
+            "hint" => "Show this help.",
+        );
     }    
+    public function getHelp(): string {
+        $message = "Here are the commands you can run:\n";
+        ksort($this->commands);
+        foreach ($this->commands as $name => $command) {
+            $message .= "$name: {$command["hint"]}\n";
+        }
+        $message .= "Multiple commands can be sent at once using ; to separate them.\n";
+        return $message;
+    }
 }
